40d08185b034e0a2073f05e9271f406c508af603
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-29 14:21:21 +0800

    Initial Commit

---
+++ 15-swift-crypto/_5_swift_cryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_swift_cryptoApp.swift
+//  15-swift-crypto
+//
+//  Created by Liu Ziyi on 29/8/23.
+//
+
+import SwiftUI
+
+@main
+struct _5_swift_cryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
