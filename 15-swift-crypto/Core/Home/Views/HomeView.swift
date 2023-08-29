868eb6212c681e679b800b0af6742b2efbf8d15e
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-29 17:01:43 +0800

    - Project Setup

---
+++ 15-swift-crypto/Core/Home/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  15-swift-crypto
+//
+//  Created by Liu Ziyi on 29/8/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
